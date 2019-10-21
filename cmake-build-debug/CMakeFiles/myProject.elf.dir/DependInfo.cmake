76774e958530168bfe3373e1213234880e6a67b3
TRANQUANGLE1004 <tranquangle1997@gmail.com>
2019-10-21 19:47:16 +0700

    first commit

---
+++ cmake-build-debug/CMakeFiles/myProject.elf.dir/DependInfo.cmake
@@ -0,0 +1,74 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/kuro/CLionProjects/myProject/startup/startup_stm32f411xe.s" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/startup/startup_stm32f411xe.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F411xE"
+  "USE_HAL_DRIVER"
+  "__packed=__attribute__((__packed__))"
+  "__weak=__attribute__((weak))"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kuro/CLionProjects/myProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "/home/kuro/CLionProjects/myProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "/home/kuro/CLionProjects/myProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "/home/kuro/CLionProjects/myProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "/home/kuro/CLionProjects/myProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "/home/kuro/CLionProjects/myProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "/home/kuro/CLionProjects/myProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "/home/kuro/CLionProjects/myProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "/home/kuro/CLionProjects/myProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "/home/kuro/CLionProjects/myProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "/home/kuro/CLionProjects/myProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "/home/kuro/CLionProjects/myProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "/home/kuro/CLionProjects/myProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "/home/kuro/CLionProjects/myProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "/home/kuro/CLionProjects/myProject/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "/home/kuro/CLionProjects/myProject/Src/main.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Src/main.c.obj"
+  "/home/kuro/CLionProjects/myProject/Src/stm32f4xx_hal_msp.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Src/stm32f4xx_hal_msp.c.obj"
+  "/home/kuro/CLionProjects/myProject/Src/stm32f4xx_it.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Src/stm32f4xx_it.c.obj"
+  "/home/kuro/CLionProjects/myProject/Src/syscalls.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Src/syscalls.c.obj"
+  "/home/kuro/CLionProjects/myProject/Src/system_stm32f4xx.c" "/home/kuro/CLionProjects/myProject/cmake-build-debug/CMakeFiles/myProject.elf.dir/Src/system_stm32f4xx.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F411xE"
+  "USE_HAL_DRIVER"
+  "__packed=__attribute__((__packed__))"
+  "__weak=__attribute__((weak))"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc"
+  "../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/myProject.elf.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj"
+  "CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj"
+  "CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj"
+  "CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj"
+  "CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj"
+  "CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj"
+  "CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj"
+  "CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj"
+  "CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj"
+  "CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj"
+  "CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj"
+  "CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj"
+  "CMakeFiles/myProject.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj"
+  "CMakeFiles/myProject.elf.dir/Src/main.c.obj"
+  "CMakeFiles/myProject.elf.dir/Src/stm32f4xx_hal_msp.c.obj"
+  "CMakeFiles/myProject.elf.dir/Src/stm32f4xx_it.c.obj"
+  "CMakeFiles/myProject.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/myProject.elf.dir/Src/system_stm32f4xx.c.obj"
+  "CMakeFiles/myProject.elf.dir/startup/startup_stm32f411xe.s.obj"
+  "myProject.elf"
+  "myProject.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/myProject.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
